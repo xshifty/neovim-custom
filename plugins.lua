e0b9736eea419729362ecadabd8e851be4ce8e95
Rafael da Silveira <rafael.dasilveira@auto1.com>
2023-12-27 11:30:49 +0100

    Neovim custom config

---
+++ plugins.lua
@@ -0,0 +1,27 @@
+local plugins = {
+ {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "gopls",
+      }
+    }
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+}
+
+return plugins

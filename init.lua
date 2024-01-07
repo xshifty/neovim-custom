81f515a2a00aca254f917df8790e589d0670fb7f
Rafael da Silveira <desenvolvedor.silveira@gmail.com>
2024-01-07 17:35:51 +0100

    add new action for code navigation

--- init.lua
+++ init.lua
@@ -1,4 +1,20 @@
 vim.opt.colorcolumn = "120"
+
+vim.api.nvim_set_keymap("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", {
+  noremap = true,
+  silent = true,
+})
+
+vim.api.nvim_set_keymap("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {
+  noremap = true,
+  silent = true,
+})
+
+vim.api.nvim_set_keymap("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>", {
+  noremap = true,
+  silent = true,
+})
+
 vim.filetype.add({
   extension = {
     templ = "templ",
